8ae258b3d19b7ee9a70db9b01b6ebd65134d0336
unknown <jonasfovergaard@gmail.com>
2019-10-28 11:40:50 +0100

    ninja quiz

---
+++ practise.js
@@ -0,0 +1,26 @@
+const correctAnswers = ["B", "B", "B", "B"];
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  let score = 0;
+  const userAnswer = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value
+  ];
+
+  userAnswer.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25;
+    }
+  });
+
+  scrollTo(0, 0);
+  result.classList.remove("d-none");
+
+  result.querySelector("span").textContent = `${score}%`;
+});
